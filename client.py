8864509c5743862188e652b609a412a3d44a1710
fabiokleis <fabiohkrc@gmail.com>
2024-05-12 02:25:49 -0300

    agora mostra as mensagens de erro

--- client.py
+++ client.py
@@ -2,15 +2,30 @@ import os
 import sys
 import socket
 import hashlib
-import multiprocessing
+import pathlib
+from multiprocessing import Process
 
 SERVER = 'localhost'
 PORT = 8123
+CHUNK_SIZE = 8192
 MAX_MESSAGE_SIZE = 1024
+
+Clients = []
+
+def create_client():
+    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # tcp socket
+    c.connect((SERVER, PORT)) # client connection
+    f = ChunkableFile() # new file
+    
+    c.send(format('client_id'))
+    f.client_id = c.recv(MAX_MESSAGE_SIZE).decode() # get base64 random client id
+
+    return c, f
 
 class ChunkableFile:
     def __init__(self):
         self.client_id = ''
+        self.server_file_name = ''
         self.file_name = ''
         self.check_sum = ''
         self.failed = False
@@ -18,83 +33,88 @@ class ChunkableFile:
         
     def write(self, data):
         if self.file_name == '':
-            return
+            self.file_name = pathlib.Path(self.server_file_name).parts[-1]
 
         if None == self.file:
             self.file = open(f'{self.file_name}.copy', 'w+b') # write read in binary
 
-        self.file.write(bytes(data, 'ascii'))
+        self.file.write(data)
 
     def check_checksum(self):
-        self.failed = self.check_sum != hashlib.file_digest(self.file, 'sha256').hexdigest()
+        self.file.seek(0) # start reading file at the beginning
+        hs256 = hashlib.sha256()
+        for chunk in iter(lambda: self.file.read(CHUNK_SIZE), b''):
+            hs256.update(chunk)
+
+        self.failed = self.check_sum != hs256.hexdigest()
 
     def close(self):
         if None != self.file and self.failed:
-            os.remove(self.file_name)
+            os.remove(f'{self.file_name}.copy')
+            print(f'failed to copy {self.file_name}')
         elif None != self.file:
+            print(f'successfully copied {self.server_file_name} to {self.file_name}.copy')
             self.file.close()
 
     def __repr__(self):
         if None != self.file:
             return self.file
-        return ''
-    
+        return self.client_id
+
+# all message sent to server must end with \r\n
 def format(message: str):
     return bytes(message + '\r\n', 'ascii')
 
-def parse_resp(f: ChunkableFile, message):
-    print('message: ', message)
-    match message[-1]:
-        case 0xa: # common message
-            if message.decode('iso-8859-1').startswith(f.file_name):
-                f.check_sum = message.decode('iso-8859-1').split(f.file_name)[1]
-                #print(f.check_sum)
-                return
-            
-            if message.decode().startswith('saindo'):
-                print('gracefully shutdown')
-                return
+# read socket and write file
+def keep_reading(c, f):
+    while True:
+        msg = c.recv(CHUNK_SIZE)
+        if msg == b'': # stop proccess when server socket was closed
+            break
+        
+        f.write(msg) # write file chunk
 
-            print(message.decode())
-            return
-
-        case _: # anything else should be a filechunk
-            
-            f.write(message)
-            
-
-def client():
-
-    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # tcp socket
-    c.connect((SERVER, PORT)) # client connection
-
-    f = ChunkableFile()
-
-    c.send(format('client_id'))
-    f.client_id = c.recv(MAX_MESSAGE_SIZE).decode('ascii') # get base64 random client id
+    print('')
+    f.check_checksum()
+    f.close()
+    c.close()
     
+        
+def main():
+    c, f = create_client()
     while True:
         try:
-            msg = input(f'{f.client_id}@{SERVER}> ')
+            msg = input(f'{f.client_id}@{SERVER}$ ')
             c.send(format(msg))
+            resp = c.recv(MAX_MESSAGE_SIZE)
 
-            if msg.startswith('file '):
-                f.file_name = msg.split(' ')[1]
-                print(f.file_name)
+            if resp[-1] == 0xa and msg != 'sair': # catch possible error message
+                print(resp.decode(), end='')
+                continue
+
+            if msg.startswith('file '):    
+                f.server_file_name = msg.split(' ')[1]
+                f.check_sum = resp.hex() # should be checksum after file message or error
+
+                p = Process(target=keep_reading, args=(c, f))
+                Clients.append(p)
+                p.start()
+                main() # new client
+
+            if resp.decode().startswith('saindo') or msg == 'sair':
+                print('gracefully shutdown')
+                f.close()
+                c.close()
+                break
             
-            parse_resp(f, c.recv(MAX_MESSAGE_SIZE))
+            print(resp.decode()) # common message
             
         except Exception as e:
             print(e)
             break
-    c.close()
 
-    return f
-        
-def main():
-    f = client()
-    print(f)
-    f.close()
+    for cp in Clients:
+        cp.join()
 
 
 if __name__ == "__main__":
