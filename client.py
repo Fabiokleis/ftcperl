1de8320ed4d31b6e79ca4a613e6665817bad8001
fabiokleis <fabiohkrc@gmail.com>
2024-05-11 23:34:44 -0300

    feito um client basico, falta separar a parte do comando file para uma thread

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
+import os
 import sys
 import socket
-import os
+import hashlib
+import multiprocessing
 
 SERVER = 'localhost'
 PORT = 8123
@@ -12,48 +14,54 @@ class ChunkableFile:
         self.file_name = ''
         self.check_sum = ''
         self.failed = False
-        self.fo = None
+        self.file = None
         
     def write(self, data):
         if self.file_name == '':
             return
-        
-        if None == self.fo:
-            self.fo = open(self.file_name, 'w')
 
-        self.fo.write(data)
+        if None == self.file:
+            self.file = open(f'{self.file_name}.copy', 'w+b') # write read in binary
 
-    def check_check_sum(self, check_sum):
-        ...
+        self.file.write(bytes(data, 'ascii'))
+
+    def check_checksum(self):
+        self.failed = self.check_sum != hashlib.file_digest(self.file, 'sha256').hexdigest()
 
     def close(self):
-        if None != self.fo and self.failed:
+        if None != self.file and self.failed:
             os.remove(self.file_name)
-        elif None != self.fo:
-            self.fo.close()
+        elif None != self.file:
+            self.file.close()
 
     def __repr__(self):
-        if None != self.fo:
-            return self.fo
+        if None != self.file:
+            return self.file
         return ''
     
 def format(message: str):
     return bytes(message + '\r\n', 'ascii')
 
-def parse_resp(f: ChunkableFile, message: str):
-    #print(message)
-    match message[-1:]:
-        case b'\n': # common message
-            print(message.decode(), end='')
-        case b'0': # client_id
-            f.client_id = message.decode()[:-2]
-        case b'1': # filechunk
-            f.write_chunk(message[1:])
-        case b'2':
-            f.check_check_sum(message[1:])
-        case _: # anything else
-            raise TypeError('unknown message')
+def parse_resp(f: ChunkableFile, message):
+    print('message: ', message)
+    match message[-1]:
+        case 0xa: # common message
+            if message.decode('iso-8859-1').startswith(f.file_name):
+                f.check_sum = message.decode('iso-8859-1').split(f.file_name)[1]
+                #print(f.check_sum)
+                return
+            
+            if message.decode().startswith('saindo'):
+                print('gracefully shutdown')
+                return
 
+            print(message.decode())
+            return
+
+        case _: # anything else should be a filechunk
+            
+            f.write(message)
+            
 
 def client():
 
@@ -62,20 +70,19 @@ def client():
 
     f = ChunkableFile()
 
-    c.send(format('client_id')) # get base64 random client id
-    parse_resp(f, c.recv(MAX_MESSAGE_SIZE))
+    c.send(format('client_id'))
+    f.client_id = c.recv(MAX_MESSAGE_SIZE).decode('ascii') # get base64 random client id
     
     while True:
         try:
             msg = input(f'{f.client_id}@{SERVER}> ')
             c.send(format(msg))
 
-            if msg == 'sair':
-                print('saindo....')
-                break
+            if msg.startswith('file '):
+                f.file_name = msg.split(' ')[1]
+                print(f.file_name)
             
-            resp = c.recv(MAX_MESSAGE_SIZE)
-            parse_resp(f, resp)
+            parse_resp(f, c.recv(MAX_MESSAGE_SIZE))
             
         except Exception as e:
             print(e)
@@ -83,7 +90,7 @@ def client():
     c.close()
 
     return f
-
+        
 def main():
     f = client()
     print(f)
